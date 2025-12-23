7ab64f3d86297e12de787d7141b348a04944d74e
Raffael Sahli <raffael.sahli@doodle.com>
2025-12-23 10:08:02 +0100

    fix: add profiles

---
+++ cmd/cli/run_profile_github_actions.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func (f *runFlags) githubActionsProfile() error {
+	if !runCmd.Flags().Changed("report") {
+		f.report = reportTypeMarkdown.String()
+	}
+
+	if !runCmd.Flags().Changed("output") {
+		renderOutputBufferDefaultTemplate = `
+	{{- $tags := "" }}
+	{{- range $tag := .Tags}}
+		{{- if eq $tags "" }}
+			{{- $tags = printf "%s=%s" $tag.Key $tag.Value }}
+		{{- else }}
+			{{- $tags = printf "%s %s=%s" $tags $tag.Key $tag.Value }}
+		{{- end }}
+	{{- end }}
+
+	{{- $stepName := .StepName }}
+	{{- if $tags }}
+		{{- $stepName = printf "%s[%s]" .StepName $tags }}
+	{{- end }}
+
+	{{- if and .Error .Skipped }}
+		{{- printf "⚠️ %s\n%s" $stepName .Buffer }}
+	{{- else if .Error }}
+		{{- printf "⛔ %s\n%s" $stepName .Buffer }}
+	{{- else }}
+		{{- printf "::group::✅ %s\n%s\n::endgroup::\n" $stepName .Buffer }}
+	{{- end }}`
+		runArgs.output = fmt.Sprintf("%s=%s", renderOutputBuffer.String(), renderOutputBufferDefaultTemplate)
+	}
+
+	if !runCmd.Flags().Changed("report-output") && os.Getenv("GITHUB_STEP_SUMMARY") != "" {
+		runArgs.reportOutput = os.Getenv("GITHUB_STEP_SUMMARY")
+	}
+
+	return nil
+}
